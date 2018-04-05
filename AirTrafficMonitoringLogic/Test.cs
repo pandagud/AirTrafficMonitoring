80306235037bb2227dc818bd259ccbe948735eb1
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-04-05 10:46:22 +0200

    Somechanges

---
+++ AirTrafficMonitoringLogic/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace AirTrafficMonitoringLogic
+{
+    public class Test
+    {
+        private TransponderReceiver.TransponderReceiverFactory receiver = new TransponderReceiverFactory();
+
+        public void getSomething()
+        {
+            var something = receiver;
+        }
+    }
+}
