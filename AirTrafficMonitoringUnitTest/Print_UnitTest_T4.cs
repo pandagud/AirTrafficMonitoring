8e51ec7f6114a85a366f57113b59a3f9f84521d5
Rikke Pilgaard <r.pilgaard@hotmail.com>
2018-04-08 15:30:01 +0200

    Unittest Print

--- AirTrafficMonitoringUnitTest/Print_UnitTest_T4.cs
+++ AirTrafficMonitoringUnitTest/Print_UnitTest_T4.cs
@@ -4,35 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitoringLogic;
+using AirTrafficMonitoringLogic.Interface;
 using NSubstitute;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using TransponderReceiver;
+
 
 namespace AirTrafficMonitoringUnitTest
 {
     [TestFixture()]
     class Print_UnitTest_T4
     {
-        private Print _print;
-        
+        private IPrint _print;
+        List<Aircraft> aircraftList = new List<Aircraft>();
 
         [SetUp]
         public void SetUp()
         {
             Aircraft aircraft1 = new Aircraft("PKY304", 94273, 54214, 12300, "20180408143635889");
             Aircraft aircraft2 = new Aircraft("LPM304", 67390, 43672, 11900, "20180409143635888");
-            List<Aircraft> aircraftList = new List<Aircraft>();
             aircraftList.Add(aircraft1);
-            aircraftList.Add(aircraft2);
+            
+            //aircraftList.Add(aircraft2);
+            
         }
         [Test]
         public void PrintData_()
         {
-            Assert.That(_print.)
+            
+            string outputline = "Tag name is: PKY304 Current position is : 94273,54214 Current altitude is: 12300 current timestamp : 20180408143635889";
+            var currentConsoleOut = Console.Out;
+
+            //using (var consoleOutput = new ConsoleOutput())
+            //{
+            //    _print.PrintData(aircraftList);
+
+            //    Assert.AreEqual(outputline, consoleOutput.GetOuput());
+            //}
+
+            Assert.AreEqual(outputline, Console.Out);
         }
-   
-
-
 }
 }
