0ae47c8fbcb4917a32344b71fdcc7576537b3af0
Morten <mo.rt.en@hotmail.com>
2018-04-12 10:59:05 +0200

    Added subject/IObserver to implement observer pattern

---
+++ AirTrafficMonitoringLogic/Utillity/SubjectObserver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoringLogic.Interface;
+
+namespace AirTrafficMonitoringLogic.Utillity
+{
+    public abstract class SubjectObserver
+    {
+        List<IObserver> ObserverList;
+
+
+        public void Attach(IObserver o)
+        {
+            ObserverList.Add(o);
+        }
+
+        public void Detach(IObserver o)
+        {
+            ObserverList.Remove(o);
+        }
+
+        public void Notify(List<Aircraft> s)
+        {
+            foreach (var item in ObserverList)
+            {
+                item.Update(s);
+            }
+        }
+
+    }
+}
