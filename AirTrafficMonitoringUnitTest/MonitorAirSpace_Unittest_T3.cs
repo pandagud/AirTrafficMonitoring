66702fa4fd8309d92143b74f84da162ecd7709b7
Morten <mo.rt.en@hotmail.com>
2018-04-19 09:13:33 +0200

    Unittest monitor

--- AirTrafficMonitoringUnitTest/MonitorAirSpace_Unittest_T3.cs
+++ AirTrafficMonitoringUnitTest/MonitorAirSpace_Unittest_T3.cs
@@ -7,31 +7,59 @@ using AirTrafficMonitoringLogic;
 using AirTrafficMonitoringLogic.Interface;
 using NSubstitute;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace AirTrafficMonitoringUnitTest
 {
+    public class TestOfMonitorClass : IObserver
+    {
+        public List<Aircraft> testList;
+        public void Update(List<Aircraft> s)
+        {
+            testList = s;
+        }
+
+        public List<Aircraft> returnList()
+        {
+            return testList;
+        }
+    }
     [TestFixture()]
     class MonitorAirSpace_Unittest_T3
     {
 
         private MonitoringAirSpace _uut;
         private List<Aircraft> AircraftList;
+        private List<Aircraft> FilteredAircraftlist;
         private string[] correctArray;
         private Aircraft _testAircraftTrue;
         private Aircraft _testAircraftFalse;
-        private IRecieveAircrafts TestRecieve;
+        private Aircraft _testAircraftOnBoundary;
+        private Aircraft _testAircraftFalseOnCoordinatesUnderAndAltitudeUnder;
+        private Aircraft _testAircraftFalseOnCoordinatesOverAndAltitudeOver;
+        private IRecieveAircrafts _testRecieve;
+        private TestOfMonitorClass _testOfMonitorClass;
 
         [SetUp]
         public void SetUp()
         {
-            TestRecieve = Substitute.For<IRecieveAircrafts>();
+            _testRecieve = Substitute.For<IRecieveAircrafts>();
             AircraftList = new List<Aircraft>();
+            FilteredAircraftlist = new List<Aircraft>();
             _testAircraftTrue = new Aircraft("ATR423", 39045, 12932, 14000, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
-            _testAircraftFalse = new Aircraft("ATR423", 39045, 12932, 14000, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
-            AircraftList.Add(_testAircraftTrue);
+            _testAircraftOnBoundary = new Aircraft("ATR422", 90000, 10000, 500, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
+            _testAircraftFalseOnCoordinatesUnderAndAltitudeUnder = new Aircraft("ATR422", 90001, 12000, 590, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
+            _testAircraftFalseOnCoordinatesOverAndAltitudeOver = new Aircraft("ATR422", 90000, 9999, 590, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
+            
             AircraftList.Add(_testAircraftFalse);
-            _uut = new MonitoringAirSpace(TestRecieve);
+            AircraftList.Add(_testAircraftTrue);
+            FilteredAircraftlist.Add(_testAircraftTrue);
+            _uut = new MonitoringAirSpace(_testRecieve);
+            _testOfMonitorClass = Substitute.For<TestOfMonitorClass>();
+            _uut.Attach(_testOfMonitorClass);
         }
+
+       
 
 
         [Test]
@@ -39,13 +67,42 @@ namespace AirTrafficMonitoringUnitTest
         {
             Assert.AreEqual(_uut.checkForAltitude(_testAircraftTrue),true);
             
-
         }
         [Test]
-        public void MonitorAirspace_CheckForAltituteTestFalse()
+        public void MonitorAirspace_CheckForAltituteTestFalseBoundaryOver()
         {
-            _uut.checkForAltitude(_testAircraftFalse);
+            Assert.AreEqual(_uut.checkForAltitude(_testAircraftFalseOnCoordinatesOverAndAltitudeOver),false);
 
         }
+        public void MonitorAirspace_CheckForAltituteTestFalseBoundaryUnder()
+        {
+            Assert.AreEqual(_uut.checkForAltitude(_testAircraftFalseOnCoordinatesUnderAndAltitudeUnder), false);
+
+        }
+
+        [Test]
+        public void MonitorAirspace_CheckForXYCoordinatestrue()
+        {
+            Assert.AreEqual(_uut.checkForXAndY(_testAircraftTrue),true);
+        }
+        [Test]
+        public void MonitorAirspace_CheckForXYCoordinatesfalseBoundaryOver()
+        {
+            Assert.AreEqual(_uut.checkForXAndY(_testAircraftFalseOnCoordinatesOverAndAltitudeOver), false);
+        }
+        [Test]
+        public void MonitorAirspace_CheckForXYCoordinatesfalseBoundaryUnder()
+        {
+            Assert.AreEqual(_uut.checkForXAndY(_testAircraftFalseOnCoordinatesUnderAndAltitudeUnder), false);
+        }
+        [Test]
+        public void MonitorAirspace_True()
+        {
+            _uut.Monitor(AircraftList);
+            Assert.AreEqual(_testOfMonitorClass.testList[0],FilteredAircraftlist[0]);
+
+        }
+        
+
     }
 }
