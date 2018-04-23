56b609e58089fa35baf3bf8c35ce1a9f8da53593
Morten <mo.rt.en@hotmail.com>
2018-04-23 12:42:50 +0200

    test

--- AirTrafficMonitoringUnitTest/HandleSeparationEvents_Unittest_T7.cs
+++ AirTrafficMonitoringUnitTest/HandleSeparationEvents_Unittest_T7.cs
@@ -16,26 +16,9 @@ namespace AirTrafficMonitoringUnitTest
         IToLogFile itlf = Substitute.For<IToLogFile>();
         ISeparationEvent ise = Substitute.For<ISeparationEvent>();
         private HandleSeparationEvents _uut;
+        [SetUp]
         public void SetUp()
         {
-            AircraftList = new List<Aircraft>();
-            AircraftList = new List<Aircraft>();
-            _testAircraft1 = new Aircraft("ATR423", 10000, 10000, 20000, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
-            _testAircraft2 = new Aircraft("ATR424", 10000, 10500, 20000, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
-            _testAircraft3 = new Aircraft("ATR425", 60000, 68738, 19700, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
-            _testAircraft4 = new Aircraft("ATR426", 58000, 23000, 14000, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
-            _testAircraft5 = new Aircraft("ATR427", 45000, 30000, 15000, DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture));
-
-
-            AircraftList.Add(_testAircraft1);
-            AircraftList.Add(_testAircraft2);
-
-            _nEventsReceived = 0;
-            List<Aircraft> _aircraftlist = new List<Aircraft>();
-            _nEventsReceived = 0;
-            _mEventsReceived = 0;
-
-
             _uut = new HandleSeparationEvents(ise,itlf);
 
 
@@ -48,5 +31,20 @@ namespace AirTrafficMonitoringUnitTest
             //};
 
         }
+
+        [Test]
+        public void HandleEvents_GettingEvent()
+        {
+
+        }
+
+        [Test]
+        public void HandleEvents_gettingNewEvent()
+        {
+
+        }
+
+
+
     }
 }
