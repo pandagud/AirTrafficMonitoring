c3d1eb917d762564b3bd12f97293de889972d94b
Bjørn Pedersen <bjoern_94@hotmail.com>
2018-04-24 09:29:54 +0200

    Created IT2 and implemented SetUp

--- AirTrafficMonitoringIntegrationTest/IT2_RecieveAircraftsToDirectionAndSpeedCalculator.cs
+++ AirTrafficMonitoringIntegrationTest/IT2_RecieveAircraftsToDirectionAndSpeedCalculator.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitoringLogic;
+using AirTrafficMonitoringLogic.Interface;
+using NSubstitute;
 using NUnit.Framework;
+using TransponderReceiver;
 
 namespace AirTrafficMonitoringIntegrationTest
 {
@@ -11,13 +15,31 @@ namespace AirTrafficMonitoringIntegrationTest
     class IT2_RecieveAircraftsToDirectionAndSpeedCalculator
     {
 
-
+        private IRecieveAircrafts _iRecieveAircrafts;
+        private ICourseAndVelocityCalculator _iCourseAndVelocityCalculator;
+        private ITransponderReceiver _iTransponderReceiver;
+        private IAirCraftObjectsUtility _iAirCraftObjectsUtility;
 
         [SetUp]
         public void SetUp()
         {
+            _iTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            _iAirCraftObjectsUtility = Substitute.For<IAirCraftObjectsUtility>();
+            _iRecieveAircrafts = new RecieveAircrafts(_iTransponderReceiver,_iAirCraftObjectsUtility);
+            _iCourseAndVelocityCalculator = Substitute.For<ICourseAndVelocityCalculator>();
+        }
+
+
+        #region RecieveAircraftsToDirectionAndSpeedCalculator
+
+        [Test]
+        public void Test1()
+        {
 
         }
+
+
+        #endregion
 
 
 
