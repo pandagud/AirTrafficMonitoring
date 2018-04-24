c8ddad9128c09c0ebf837cc049057dbfc9c0400f
Rikke Pilgaard <r.pilgaard@hotmail.com>
2018-04-24 10:33:02 +0200

    IT3 IT8

--- AirTrafficMonitoringIntegrationTest/IT3.cs
+++ AirTrafficMonitoringIntegrationTest/IT3.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitoringLogic;
+using AirTrafficMonitoringLogic.AircraftUtillity;
 using AirTrafficMonitoringLogic.Interface;
 using NUnit.Framework;
+using NSubstitute;
+using TransponderReceiver;
 
 namespace AirTrafficMonitoringIntegrationTest
 {
@@ -15,18 +18,47 @@ namespace AirTrafficMonitoringIntegrationTest
         private IAirCraftObjectsUtility _airCraftObjectsUtility;
         private IRecieveAircrafts _recieveAircrafts;
         private IMonitoringAirSpace _monitoringAirSpace;
+        private ICourseAndVelocityCalculator _courseAndVelocityCalculator;
+        private ITransponderReceiver _receiver;
+        private int _nEventsReceived;
+        private int _mEvnetsReceived;
+        private List<string> _list;
+        private List<Aircraft> _aircraftlist;
 
         [SetUp]
         public void Setup()
         {
-            _airCraftObjectsUtility = Substitute.For<IAirCraftObjectsUtility>();
-            _recieveAircrafts = new RecieveAircrafts();
-            _monitoringAirSpace= new MonitoringAirSpace();
+            _airCraftObjectsUtility = new AircraftObjectsUtility();
+            _recieveAircrafts = new RecieveAircrafts(_receiver, _airCraftObjectsUtility);
+            _monitoringAirSpace= new MonitoringAirSpace(_recieveAircrafts);
+            _courseAndVelocityCalculator = new CourseAndVelocityCalculator();
+            
+
+
+            _nEventsReceived = 0;
+            _mEvnetsReceived = 0;
+
+            _receiver.TransponderDataReady += (o, args) =>
+            {
+                _list = args.TransponderData;
+                _aircraftlist = _airCraftObjectsUtility.getListofAircraftObjects(_list);
+
+                _nEventsReceived++;
+            };
+            _recieveAircrafts.TransponderDataObjectReady += (args) =>
+            {
+
+                _aircraftlist = _airCraftObjectsUtility.getListofAircraftObjects(_list);
+                _mEvnetsReceived++;
+            };
         }
 
         [Test]
+        public void MonitoringAirSpace_Test_of_Update_And_retun_of_list()
         {
-
+            var args = new RawTransponderDataEventArgs(new List<string> { "VBF451;67000;28912;7300;20180408143814504" });
+            _receiver.TransponderDataReady += Raise.EventWith(args);
+            Assert.That(_monitoringAirSpace.Monitor());
         }
     }
 }
