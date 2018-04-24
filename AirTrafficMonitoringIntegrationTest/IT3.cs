be0800446a09e7baaa4da260007f835fc62d160f
Rikke Pilgaard <r.pilgaard@hotmail.com>
2018-04-24 09:01:51 +0200

    IT3

--- AirTrafficMonitoringIntegrationTest/IT3.cs
+++ AirTrafficMonitoringIntegrationTest/IT3.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitoringLogic;
+using AirTrafficMonitoringLogic.Interface;
+using NUnit.Framework;
 
 namespace AirTrafficMonitoringIntegrationTest
 {
-    class IT3
+    [TestFixture]
+    public class IT3
     {
+        private IAirCraftObjectsUtility _airCraftObjectsUtility;
+        private IRecieveAircrafts _recieveAircrafts;
+        private IMonitoringAirSpace _monitoringAirSpace;
+
+        [SetUp]
+        public void Setup()
+        {
+            _airCraftObjectsUtility = Substitute.For<IAirCraftObjectsUtility>();
+            _recieveAircrafts = new RecieveAircrafts();
+            _monitoringAirSpace= new MonitoringAirSpace();
+        }
+
+        [Test]
+        {
+
+        }
     }
 }
