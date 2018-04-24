bc4e4bf370cc6f013a3b384ea676fa08b50da5b2
Morten <mo.rt.en@hotmail.com>
2018-04-24 11:58:13 +0200

    It7 Changes

--- AirTrafficMonitoringIntegrationTest/IT7.cs
+++ AirTrafficMonitoringIntegrationTest/IT7.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitoringLogic;
+using AirTrafficMonitoringLogic.AircraftUtillity;
 using NUnit.Framework.Internal;
 using NUnit.Framework;
 using AirTrafficMonitoringLogic.Interface;
 using NSubstitute;
+using TransponderReceiver;
 
 namespace AirTrafficMonitoringIntegrationTest
 {
@@ -15,15 +17,30 @@ namespace AirTrafficMonitoringIntegrationTest
     class IT7
     {
         private IToLogFile _iToLogFile;
-        private ISeparationEvent _iSeparationEvent;
-        private IHandleSeparationEvents _handleSeparationEvents;
+        private CreateSeparationEvents _SeparationEvent;
+        private HandleSeparationEvents _handleSeparationEvents;
+        private IRecieveAircrafts _iRecieveAircrafts;
+        private IMonitoringAirSpace _ImonitoringAirSpace;
+        private IAirCraftObjectsUtility _iAirCraftObjectsUtility;
+        private IObserverSepArgs _iObsSepArgs;
+        private IObserver _iObs;
+        private ICourseAndVelocityCalculator _iCourseAndVelocityCalculator;
+        private ITransponderReceiver _iTransponderReceiver;
+
 
         [SetUp]
         public void Setup()
         {
             _iToLogFile = Substitute.For<IToLogFile>();
-            _iSeparationEvent = new CreateSeparationEvents();
-            _handleSeparationEvents = new HandleSeparationEvents(_iSeparationEvent,_iToLogFile);
+            _SeparationEvent = new CreateSeparationEvents();
+            _iCourseAndVelocityCalculator = new CourseAndVelocityCalculator();
+            _iAirCraftObjectsUtility = new AircraftObjectsUtility();
+            _iTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            _iRecieveAircrafts = new RecieveAircrafts(_iTransponderReceiver,_iAirCraftObjectsUtility,_iCourseAndVelocityCalculator);
+            _ImonitoringAirSpace = new MonitoringAirSpace(_iRecieveAircrafts);
+            
+            _handleSeparationEvents = new HandleSeparationEvents(_SeparationEvent, _iToLogFile);
+            _SeparationEvent.Attach(_handleSeparationEvents);
         }
 
         [Test]
