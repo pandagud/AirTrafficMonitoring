45dbc3c33ee00baf2466f4bc7f33dc72009cf022
Bjørn Pedersen <bjoern_94@hotmail.com>
2018-04-24 09:00:01 +0200

    IT2 renamed

--- AirTrafficMonitoringIntegrationTest/IT2_RecieveAircraftsToDirectionAndSpeedCalculator.cs
+++ AirTrafficMonitoringIntegrationTest/IT2_RecieveAircraftsToDirectionAndSpeedCalculator.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace AirTrafficMonitoringIntegrationTest
 {
-    class IT2
+    [TestFixture]
+    class IT2_RecieveAircraftsToDirectionAndSpeedCalculator
     {
+
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+
+
+
+
     }
 }
